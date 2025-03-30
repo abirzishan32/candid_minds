58b30f35d0061834f55088283d7dcbe8ccc38e79
abir_zishan <rahman2007026@stud.kuet.ac.bd>
2025-03-30 07:10:47 +0600

    feat: enhance AuthForm component with validation and error handling

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -5,36 +5,47 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+    Form
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import Link from "next/link";
+import {toast} from "sonner"
 
-const formSchema = z.object({
-    username: z.string().min(2).max(50),
-});
+
+
+const authFormSchema = (type: FormType) => {
+    return z.object({
+      name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
+      email: z.string().email(),
+      password: z.string().min(3),
+    });
+  };
 
 const AuthForm = ({ type }: { type: FormType }) => {
-    // 1. Define your form.
+    const formSchema = authFormSchema(type);
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            username: "",
+          name: "",
+          email: "",
+          password: "",
         },
-    });
+      });
 
     // 2. Define a submit handler.
     function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values);
+        try{
+            if (type === 'sign-up') {
+                
+            }
+            else{
+                
+            }
+        }
+        catch(error){
+            console.log(error)
+            toast.error("There was an error: ${error.message}")
+        }
     }
 
     const isSignIn = type === 'sign-in';
