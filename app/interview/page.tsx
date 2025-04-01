88c05127aeef51129199e29e4b06968cac190dec
abir_zishan <rahman2007026@stud.kuet.ac.bd>
2025-04-02 02:14:28 +0600

    interview home page done (With some errors to fix)

--- app/interview/page.tsx
+++ app/interview/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
+import {dummyInterviews} from "@/constants";
+import InterviewCard from "@/components/InterviewCard";
 
 const Page = () => {
     return (
@@ -21,7 +23,10 @@ const Page = () => {
             <section className="flex flex-col gap-6 mt-8">
                 <h2> Your Interviews</h2>
                 <div className="interviews-section">
-                    <p>You haven't taken any interviews yet!</p>
+                    {dummyInterviews.map((interview) => (
+                        <InterviewCard {...interview} />
+                    ))}
+                    {/*<p>You haven't taken any interviews yet!</p>*/}
                 </div>
 
             </section>
@@ -30,7 +35,10 @@ const Page = () => {
                 <h2> Take an interview</h2>
 
                 <div className="interviews-section">
-                    <p> There are no interviews available</p>
+                    {dummyInterviews.map((interview) => (
+                        <InterviewCard {...interview} />
+                    ))}
+                    {/*<p> There are no interviews available</p>*/}
                 </div>
             </section>
 
