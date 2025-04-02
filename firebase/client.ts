c89adfde2ff1064badb1d9fb4e631faa14c5fbbf
abir_zishan <rahman2007026@stud.kuet.ac.bd>
2025-04-03 02:25:40 +0600

    Firebase setup initialization for both Admin and Client end done

---
+++ firebase/client.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import {getAuth} from "firebase/auth"
+import {getFirestore} from "firebase-admin/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCPPguYa1nb6_xWzzqz-fWwEPzvZpXMtQc",
+    authDomain: "candid-minds.firebaseapp.com",
+    projectId: "candid-minds",
+    storageBucket: "candid-minds.firebasestorage.app",
+    messagingSenderId: "666456410071",
+    appId: "1:666456410071:web:8ccca5749c12ab4b00ad92",
+    measurementId: "G-0KBW5ZTLCR"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+
+
+export const auth = getAuth(app)
+export const db = getFirestore(app)
