4ca678cbd9d3911e8f8ef51f6fc0fb7d95564ab1
abir_zishan <rahman2007026@stud.kuet.ac.bd>
2025-04-04 08:39:29 +0600

    API to connect between Vapi assistant to Gemini LLM done (get and post request)

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -23,7 +23,7 @@ export async function POST(request: Request){
             The amount of questions required is: ${amount}.
             Please return only the questions, without any additional text.
             The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
-            Return the questions formatted like this:
+            Return the questions formatted like the following:
             ["Question 1", "Question 2", "Question 3"]
         `,
 
